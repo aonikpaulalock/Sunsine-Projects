90fa75b327933c3d985314f51b63437970ce3c90
aonikpaulalock <alockpaul9845@gmail.com>
2022-04-18 14:12:26 +0600

    Projects Done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,11 +5,12 @@ const About = () => {
     <div className='container'>
       <div className="row">
         <div className="col-md-6 mt-sm-5 mt-2 pt-sm-5 pt-2 order-sm-1 order-2">
-        <div className="pt-sm-5 pt-2 content">
-        <h1>My Goal</h1>
-          <p className="py-3 fs-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, molestiae corporis vero id esse ad asperiores facilis soluta animi necessitatibus consequuntur tempora? Vitae, vel rem. Minima alias debitis quasi a!</p>
-          <button className="Hire-BTN">See-More</button>
-        </div>
+          <div className="pt-sm-5 pt-2 content">
+            <h1>My Goal</h1>
+            <p className="py-3 fs-5">My goal is to be a good quality developer. i will try all time learning and practice do. i will try dailly 10-12 hour practicing .then achive my goal because practice makes man perfect
+            </p>
+            <button className="Hire-BTN">See-More</button>
+          </div>
         </div>
         <div className="col-md-6 order-sm-2 order-1">
           <img src={about} alt="" className="img-fluid" />
